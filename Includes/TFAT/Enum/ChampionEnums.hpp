bb3008e2324c54e6b52500d70d40b039ff6f256c
snowapril <sinjihng@naver.com>
2020-06-26 11:01:48 +0000

    feat : implement macro for converting string to enum

--- Includes/TFAT/Enum/ChampionEnums.hpp
+++ Includes/TFAT/Enum/ChampionEnums.hpp
@@ -15,28 +15,30 @@ namespace TFAT {
 
     enum class ChampionStatTag 
     {
-#define X(a) a
+#define X(a) a,
 #include <TFAT/Enum/ChampionStatTagEnum.def>
 #undef X
+        INVALID
     };
 
     const std::string CHAMPION_STAT_TAG_STR[] 
     {
-#define X(a) #a
+#define X(a) #a,
 #include <TFAT/Enum/ChampionStatTagEnum.def>
 #undef X
     };
 
     enum class ChampionOrigin
     {
-#define X(a) a
+#define X(a) a,
 #include <TFAT/Enum/ChampionOriginEnum.def>
 #undef X
+        INVALID
     };
 
     const std::string CHAMPION_ORIGIN_STR[]
     {
-#define X(a) #a
+#define X(a) #a,
 #include <TFAT/Enum/ChampionOriginEnum.def>
 #undef X
     };
@@ -44,31 +46,56 @@ namespace TFAT {
 
     enum class ChampionType
     {
-#define X(a) a
+#define X(a) a,
 #include <TFAT/Enum/ChampionTypeEnum.def>
 #undef X
+        INVALID
     };
 
     const std::string CHAMPION_TYPE_STR[]
     {
-#define X(a) #a
+#define X(a) #a,
 #include <TFAT/Enum/ChampionTypeEnum.def>
 #undef X
     };
     
     enum class ChampionSkillType
     {
-#define X(a) a
+#define X(a) a,
 #include <TFAT/Enum/ChampionSkillTypeEnum.def>
 #undef X 
+        INVALID
     };
 
     const std::string CHAMPION_SKILL_TYPE_STR[]
     {
-#define X(a) #a
+#define X(a) #a,
 #include <TFAT/Enum/ChampionSkillTypeEnum.def>
 #undef X 
     };
+
+    template <typename Enum>
+    Enum StrToEnum(const std::string& str)
+    {
+        static_assert("No implementation exists.");
+    }
+
+#define STR2ENUM(ENUM, STR_ARRAY) \
+    template<> \
+    inline ENUM StrToEnum(const std::string& str) \
+    { \
+        for (int i = 0; i < static_cast<int>(ENUM::INVALID); ++i) \
+        { \
+            if (str == STR_ARRAY[i]) \
+                return static_cast<ENUM>(i); \
+        } \
+        return ENUM::INVALID; \
+    }
+
+STR2ENUM(ChampionOrigin, CHAMPION_ORIGIN_STR)
+STR2ENUM(ChampionSkillType, CHAMPION_SKILL_TYPE_STR)
+STR2ENUM(ChampionStatTag, CHAMPION_STAT_TAG_STR)
+STR2ENUM(ChampionType, CHAMPION_TYPE_STR)
 }
 
 #endif
